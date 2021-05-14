5f9b7a9214a0833cbcf0df699e4319fc85f1b52f
Marcos Beraldo Barros <marcosberaldobarros@hotmail.com>
2021-05-13 22:24:37 -0400

    Criando cadastramento de produtos e interação com banco de dados
    Usando imagens e banco de dados MySQL

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Produto
 
-# Register your models here.
+@admin.register(Produto)
+class ProdutoAdmim(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'estoque', 'slug', 'criado', 'modificado', 'ativo')
--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from stdimage.models import StdImageField
+
+#SIGNALS
+from django.db.models import signals
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+class Base(models.Model):
+    criado = models.DateField('Data de criação', auto_now_add=True)
+    modificado = models.DateField('Data de Atuação', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+class Produto(Base):
+    nome = models.CharField('Nome',max_length=100)
+    preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
+    estoque = models.IntegerField('Estoque')
+    imagem = StdImageField('Imagem', upload_to='Produtos', variations={'thumb': (124, 124)})
+    slug = models.SlugField('Slug', max_length=100, blank=True, editable=False)
+
+    def __str__(self):
+        return self.nome
+
+def produto_pre_save(signal, instance, sender, **kwargs):
+    instance.slug = slugify(instance.nome)
+
+signals.pre_save.connect(produto_pre_save,sender=Produto)
